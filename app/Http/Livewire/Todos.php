b92b01cc120a0626ba46b857a8bbed272d977ecf
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2021-02-17 14:14:35 +0100

    chore: add filter component

--- app/Http/Livewire/Todos.php
+++ app/Http/Livewire/Todos.php
@@ -8,16 +8,27 @@ use App\Models\Todo;
 class Todos extends Component
 {
     public $todo_text;
+    public $edit_mode;
     public $validation_message = "";
+    public $edited_todo_id = 0;
+    public $active_filter = "undone";
 
     protected $rules = [
         'todo.todo_text' => 'required'
     ];
 
-
-    public function todos()
+    public function getTodos()
     {
-        return Todo::all();
+        switch ($this->active_filter) {
+            case "done":
+                return Todo::where('is_done', true)->get();
+            case "undone":
+                return Todo::where('is_done', false)->get();
+            case "all":
+                return Todo::all();
+            default:
+                return Todo::all();
+        }
     }
 
     public function addTodo()
@@ -29,6 +40,7 @@ class Todos extends Component
         $this->validation_message = "";
 
         Todo::create(["todo_text" => $this->todo_text, "is_done" => false]);
+        $this->todo_text = "";
     }
 
     public function setAsDone($id)
@@ -38,8 +50,36 @@ class Todos extends Component
         $todo->save();
     }
 
+    public function setEditMode($id)
+    {
+        $todo = Todo::find($id);
+        $this->edit_mode = true;
+        $this->todo_text = $todo->todo_text;
+        $this->edited_todo_id = $todo->id;
+    }
+
+    public function updateTodo()
+    {
+        $todo = Todo::find($this->edited_todo_id);
+        $todo->todo_text = $this->todo_text;
+        $todo->save();
+        $this->edit_mode = false;
+        $this->todo_text = "";
+    }
+
+    public function removeTodo($id)
+    {
+        $todo = Todo::find($id);
+        $todo->forceDelete();
+    }
+
+    public function setActiveFilter($filter)
+    {
+        $this->active_filter = $filter;
+    }
+
     public function render()
     {
-        return view('livewire.todos', ["todos" => $this->todos()]);
+        return view('livewire.todos', ["todos" => $this->getTodos()]);
     }
 }
--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -1,26 +1,43 @@
-<div class="container">
-    <form wire:submit.prevent="addTodo">
-        <p style="color:red">{{$validation_message}}</p>
-        <input autofocus type="text" wire:model="todo_text" />
-        <button type="submit">Add</button>
+<div class="container my-5">
+    <form class="form-inline" wire:submit.prevent="{{$edit_mode ? "updateTodo": "addTodo"}}">
+        <div class="form-group">
+            <p style="color:red">{{$validation_message}}</p>
+            <label for="todo-text">Something to do</label>
+            <input class="form-control mx-4" id="todo-text" autofocus type="text" wire:model="todo_text" />
+            <button class="btn btn-primary justify-self-right" type="submit">{{$edit_mode ? "Update": "Add"}}</button>
+        </div>
     </form>
+
     <div>
+        <div class="d-flex align-items-baseline">
+            <p>Filters</p>
+
+            <div class="btn-group my-4 ml-3" role="group" aria-label="Basic example">
+                <button wire:click="setActiveFilter('undone')" type="button" aria-pressed="true" class="btn btn-sm btn-outline-secondary {{$active_filter === 'undone' ? 'active': ''}}">Undone</button>
+                <button wire:click="setActiveFilter('done')" type="button" aria-pressed="false" class="btn btn-sm btn-outline-secondary {{$active_filter === 'done' ? 'active': ''}}">Done</button>
+                <button wire:click=" setActiveFilter('all')" type="button" aria-pressed="false" class="btn btn-sm btn-outline-secondary {{$active_filter === 'all' ? 'active': ''}}">All</button>
+            </div>
+        </div>
+
+
         @foreach($todos as $todo)
-        <div class="d-flex align-items-baseline my-2">
-            @if($todo->is_done)
-            <s>
-                <p class="mx-4 d-inline">{{$todo->todo_text}}</p>
-            </s>
-            <input checked value="$todo->is_done" wire:change="setAsDone({{$todo->id}})" type="checkbox" />
-            @else
-            <p class="mx-4">{{$todo->todo_text}}</p>
-            <input value="$todo->is_done" wire:change="setAsDone({{$todo->id}})" type="checkbox" />
-            @endif
+        <div class=" d-flex align-items-baseline my-2">
+            <input {{ $todo->is_done ? "checked":  "" }} value="$todo->is_done" wire:change="setAsDone({{$todo->id}})" type="checkbox" />
+            <p class="mx-4 {{$todo->is_done ? "strikethrough":  ""}}">{{$todo->todo_text}}</p>
+
+            <div class="d-flex justify-content-between align-items-center">
+                <button>
+                    <img src="/icons/edit-icon.svg" alt="edit todo" class="mr-2" style="width:15px; cursor: pointer" wire:click="setEditMode({{$todo->id}})">
+                </button>
+                <button wire:click="removeTodo({{$todo->id}})" type="button" class="close text-danger" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
         </div>
-        <div>
-            <button wire:click="removeTodo($todo->id)">X</button>
-        </div>
+        @endforeach
+
+        @if(!count($todos))
+        <p>Nothing to do</p>
+        @endif
     </div>
-    @endforeach
-</div>
 </div>
